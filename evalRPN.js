7d21035f67fde6641fdcb142726bafbdde3aa9dc
grant.darling <grantcarterdarling@gmail.com>
2023-09-21 09:55:36 -0400

    - completed solution for evalRPN problem

--- evalRPN.js
+++ evalRPN.js
@@ -4,25 +4,21 @@
  */
 var evalRPN = function(tokens) {
     let stack = [];
+    let operators = {
+        '+': (a,b) => a+b,
+        '-': (a,b) => b-a,
+        '*': (a,b) => a*b,
+        '/': (a,b) => b/a >= 0 ? Math.floor(b/a) : Math.ceil(b/a),
+    }
 
-    for (let token of tokens) {
-        if(token === '+') {
-            stack.push(stack.pop() + stack.pop())
-        }else if(token === '-') {
-            const a = stack.pop()
-            const b = stack.pop()
-            stack.push(b - a)
-        }else if(token === '*') {
-            stack.push(stack.pop() * stack.pop())
-        }else if(token === '/') {
-            const a = stack.pop()
-            const b = stack.pop()
-            const result = (b / a) >= 0 ? Math.floor(b / a) : Math.ceil(b / a)
-            stack.push(result)
+    for(let token of tokens) {
+        if(operators[token]) {
+            const a = stack.pop();
+            const b = stack.pop();
+            stack.push(operators[token](a,b))
         } else {
             stack.push(Number(token))
         }
     }
-
     return stack.pop()
 };
