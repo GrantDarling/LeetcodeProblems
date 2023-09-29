a4ec0f28fc4382339a01173560f7a2dccc96a751
grant.darling <grantcarterdarling@gmail.com>
2023-09-28 23:09:17 -0400

    - completed solution for two sum problem

---
+++ twoSumII.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let i = 0;
+    let j = numbers.length - 1;
+    let answer;
+    while(true) {
+        answer = numbers[i] + numbers[j];
+        if(answer > target) j--;
+        if(answer < target) i++;
+        if(answer === target)
+            return [i+1, j+1]
+    }
+};
+
+twoSum([1,4,5,6,7], 7)
