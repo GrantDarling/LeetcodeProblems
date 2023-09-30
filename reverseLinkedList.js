fe0aa186f0d571f773921f87f187231523fbb3a2
grant.darling <grantcarterdarling@gmail.com>
2023-09-29 20:16:13 -0400

    - completed solution for reverse linked list

---
+++ reverseLinkedList.js
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+// recursive
+var reverseList = function(head) {
+    if (head == null || head.next == null) return head;
+
+    const result = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    return result;
+};
+
+class ListNode {
+    constructor(data) {
+        this.data = data
+        this.next = null
+    }
+}
+
+let node1 = new ListNode(1)
+let node2 = new ListNode(2)
+let node3 = new ListNode(3)
+let node4 = new ListNode(4)
+let node5 = new ListNode(5)
+let node6 = new ListNode(6)
+let node7 = new ListNode(7)
+let node8 = new ListNode(8)
+let node9 = new ListNode(9)
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+node5.next = node6
+node6.next = node7
+node7.next = node8
+node8.next = node9
+
+const answer = reverseList(node1)
+console.log(answer)
